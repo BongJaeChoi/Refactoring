c6a79185eed1a8919af137697ef6bd0eb4c82690
dashfordeath <jjgod0124@gmail.com>
2015-06-02 20:11:55 +0900

    Initial commit

---
+++ rename/translateX.js
@@ -0,0 +1,18 @@
+function translate(welTarget, nDist, nVelocity){
+    var htTranslate = {};
+
+    nVelocity = nVelocity || 0;
+
+    htTranslate.transform = 'translateX('+ nDist +'px)';
+    htTranslate.transition = 'All '+ nVelocity + 'ms ease';
+''
+    if(nVelocity === 0){
+        htTranslate.transition = '';
+    }
+
+    welTarget.css(htTranslate);
+}
+$.(document).ready(function(){
+    console.dir("test hello");
+});
+
